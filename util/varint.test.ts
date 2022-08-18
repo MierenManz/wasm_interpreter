6663cfbff0f7a9d22e4f6133a3626e3e400f1d59
MierenManz <christiaanmens@gmail.com>
2022-08-18 05:19:00 -0400

    add signed varint

--- util/varint.test.ts
+++ util/varint.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals, assertThrows } from "../_test_deps.ts";
 import { Reader } from "./reader.ts";
-import { decodeVarint } from "./varint.ts";
+import { decodeSignedVarint, decodeVarint } from "./varint.ts";
 
 Deno.test({
   name: "Varint decode",
@@ -29,6 +29,31 @@ Deno.test({
         );
 
         assertThrows(() => decodeVarint(reader), "VarInt is too big");
+      },
+    });
+  },
+});
+
+Deno.test({
+  name: "Signed Varint decode",
+  fn: async (t) => {
+    await t.step({
+      name: "Decode: 1 byte",
+      fn: () => {
+        let reader = new Reader(Uint8Array.of(0x76));
+        assertEquals(decodeSignedVarint(reader), -10);
+        reader = new Reader(Uint8Array.of(0x0A));
+        assertEquals(decodeSignedVarint(reader), 10);
+      },
+    });
+
+    await t.step({
+      name: "Decode: 2 byte",
+      fn: () => {
+        let reader = new Reader(Uint8Array.of(0x98, 0x78));
+        assertEquals(decodeSignedVarint(reader), -1000);
+        reader = new Reader(Uint8Array.of(0xE8, 0x07));
+        assertEquals(decodeSignedVarint(reader), 1000);
       },
     });
   },

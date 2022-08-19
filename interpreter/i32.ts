debf50c220fe439878cfd86dd69f3a3247efea54
MierenManz <christiaanmens@gmail.com>
2022-08-18 23:31:18 -0400

    add extra instruction

--- interpreter/i32.ts
+++ interpreter/i32.ts
@@ -1,5 +1,11 @@
+import type { FunctionContext } from "../types/compiled.ts";
 import type { Compiled } from "../types/mod.ts";
 import type { Reader } from "../util/reader.ts";
+import { decodeSignedVarint } from "../util/varint.ts";
+
+const U32MAX = 0xFFFFFFFF;
+const I32MAX = (U32MAX - 1) / 2;
+const I32MIN = I32MAX - U32MAX;
 
 export function add(
   _module: Compiled.Module,
@@ -8,11 +14,89 @@ export function add(
 ) {
   const a = context.stack.pop();
   const b = context.stack.pop();
-  if (!a || !b) throw new Error("Expected i32. Found void");
-  if (a.kind !== "i32" || b.kind !== "i32") {
-    throw new Error(`Expected [i32, i32]. Found [${a.kind}, ${b.kind}]`);
+  if (!a || !b || a.kind !== "i32" || b.kind !== "i32") {
+    throw new Error(
+      `Expected [i32, i32]. Found [${a?.kind ?? "void"}, ${b?.kind ?? "void"}]`,
+    );
   }
 
-  const res: Compiled.Value = { kind: "i32", value: a.value + b.value };
+  const val = a.value + b.value;
+  if (val < I32MIN) throw new RangeError("Byte underflow");
+  if (val > I32MAX) throw new RangeError("Byte overflow");
+
+  const res: Compiled.Value = { kind: "i32", value: val };
   context.stack.push(res);
 }
+
+export function sub(
+  _module: Compiled.Module,
+  _reader: Reader,
+  context: Compiled.FunctionContext,
+) {
+  const a = context.stack.pop();
+  const b = context.stack.pop();
+  if (!a || !b || a.kind !== "i32" || b.kind !== "i32") {
+    throw new Error(
+      `Expected [i32, i32]. Found [${a?.kind ?? "void"}, ${b?.kind ?? "void"}]`,
+    );
+  }
+
+  const val = a.value - b.value;
+  if (val < I32MIN) throw new RangeError("Byte underflow");
+  if (val > I32MAX) throw new RangeError("Byte overflow");
+
+  const res: Compiled.Value = { kind: "i32", value: val };
+  context.stack.push(res);
+}
+
+export function mul(
+  _module: Compiled.Module,
+  _reader: Reader,
+  context: Compiled.FunctionContext,
+) {
+  const a = context.stack.pop();
+  const b = context.stack.pop();
+  if (!a || !b || a.kind !== "i32" || b.kind !== "i32") {
+    throw new Error(
+      `Expected [i32, i32]. Found [${a?.kind ?? "void"}, ${b?.kind ?? "void"}]`,
+    );
+  }
+
+  const val = a.value * b.value;
+  if (val < I32MIN) throw new RangeError("Byte underflow");
+  if (val > I32MAX) throw new RangeError("Byte overflow");
+
+  const res: Compiled.Value = { kind: "i32", value: val };
+  context.stack.push(res);
+}
+
+export function div(
+  _module: Compiled.Module,
+  _reader: Reader,
+  context: Compiled.FunctionContext,
+) {
+  const a = context.stack.pop();
+  const b = context.stack.pop();
+  if (!a || !b || a.kind !== "i32" || b.kind !== "i32") {
+    throw new Error(
+      `Expected [i32, i32], Found [${a?.kind ?? "void"}, ${b?.kind ?? "void"}]`,
+    );
+  }
+
+  const val = Math.trunc(a.value / b.value);
+  if (val < I32MIN) throw new RangeError("Byte underflow");
+  if (val > I32MAX) throw new RangeError("Byte overflow");
+
+  const res: Compiled.Value = { kind: "i32", value: val };
+  context.stack.push(res);
+}
+
+function constant(
+  _module: Compiled.Module,
+  reader: Reader,
+  context: FunctionContext,
+) {
+  context.stack.push({ kind: "i32", value: decodeSignedVarint(reader) });
+}
+
+export { constant as const };
--- interpreter/local.ts
+++ interpreter/local.ts
@@ -18,3 +18,48 @@ export function get(
   // Deep copy to push onto the stack
   context.stack.push(structuredClone(val));
 }
+
+export function set(
+  _module: Compiled.Module,
+  reader: Reader,
+  context: Compiled.FunctionContext,
+) {
+  const ptr = decodeVarint(reader);
+  if (ptr >= context.locals.length || ptr < 0) {
+    throw new RangeError("Invalid Index");
+  }
+  const local = context.locals[ptr];
+
+  const v = context.stack.pop();
+  if (!v || v.kind !== local.kind) {
+    throw new TypeError(
+      `Expected Type ${local.kind}, Found ${v?.kind ?? "void"}`,
+    );
+  }
+
+  // Set value of local to value of v
+  context.locals[ptr].value = v.value;
+}
+
+export function tee(
+  _module: Compiled.Module,
+  reader: Reader,
+  context: Compiled.FunctionContext,
+) {
+  const ptr = decodeVarint(reader);
+  if (ptr >= context.locals.length || ptr < 0) {
+    throw new RangeError("Invalid Index");
+  }
+  const local = context.locals[ptr];
+
+  // v is a Reference. It's still on the stack
+  const v = context.stack.at(-1);
+  if (!v || v.kind !== local.kind) {
+    throw new TypeError(
+      `Expected Type ${local.kind}, Found ${v?.kind ?? "void"}`,
+    );
+  }
+
+  // Safe to do because `v.value` is copied by value, not by reference;
+  local.value = v.value;
+}

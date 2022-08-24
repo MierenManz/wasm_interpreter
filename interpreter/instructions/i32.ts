77f5ef059228f67ceda46b981b9bc4609d3e4f4d
MierenManz <christiaanmens@gmail.com>
2022-08-24 18:06:51 +0200

    add rotl

--- interpreter/instructions/i32.ts
+++ interpreter/instructions/i32.ts
@@ -6,6 +6,10 @@ import { decodeSignedVarint } from "../../util/varint.ts";
 import type { Compiled } from "../../types/mod.ts";
 import type { Reader } from "../../util/reader.ts";
 
+const U32MAX = 0xFFFFFFFF;
+const I32MAX = (U32MAX - 1) / 2;
+const I32MIN = I32MAX - U32MAX;
+
 // Arithmetic Operations
 export const add = generateIntegerArithmeticOp("i32", "+");
 export const sub = generateIntegerArithmeticOp("i32", "-");
@@ -20,6 +24,31 @@ export const xor = generateIntegerArithmeticOp("i32", "^");
 export const shl = generateIntegerArithmeticOp("i32", "<<");
 export const shrS = generateIntegerArithmeticOp("i32", ">>", true);
 export const shrU = generateIntegerArithmeticOp("i32", ">>", false);
+export function rotl(
+  _module: Compiled.Module,
+  _reader: Reader,
+  context: Compiled.FunctionContext,
+) {
+  const a = context.stack.pop();
+  const b = context.stack.pop();
+  if (!a || !b || a.kind !== "i32" || b.kind !== "i32") {
+    throw new Error(
+      `Expected [i32, i32], but found [${a?.kind ?? "void"}, ${
+        b?.kind ?? "void"
+      }]`,
+    );
+  }
+
+  // const valueA = a!.value;
+  // const valueB = b!.value;
+  // const highBits = valueA >>> (32 - valueB);
+  // const lowBits = valueA << valueB;
+  a.value = a.value << b.value | a.value >>> (32 - b.value);
+
+  if (a.value < I32MIN) throw new RangeError("Byte underflow");
+  if (a.value > I32MAX) throw new RangeError("Byte overflow");
+  context.stack.push(a);
+}
 // export const rotl;
 // export const rotr;
 // export const clz;

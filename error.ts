607151ef40cc1a7b08f6ee40d13ebf16bff47060
MierenManz <christiaanmens@gmail.com>
2023-01-17 22:14:16 +0100

    implement better errors

--- error.ts
+++ error.ts
@@ -1,7 +1,29 @@
 export class ParsingError extends Error {
   name = "Parsing Error";
 
-  constructor(message: string, options: ErrorOptions) {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super(message, options);
+  }
+}
+
+export class RuntimeError extends Error {
+  name = "RuntimeError";
+  constructor(message: string, options: ErrorOptions = {}) {
+    super(message, options);
+  }
+}
+
+export class DecodingError extends Error {
+  name = "DecodingError";
+  constructor(message: string, options: ErrorOptions = {}) {
+    super(message, options);
+  }
+}
+
+export class ValidationError extends Error {
+  name = "ValidationError";
+
+  constructor(message: string, options: ErrorOptions = {}) {
     super(message, options);
   }
 }

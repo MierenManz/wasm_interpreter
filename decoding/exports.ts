f52dbc985ba1745aae111e44786f0901faca9e97
MierenManz <christiaanmens@gmail.com>
2023-01-17 22:26:21 +0100

    implement exportSection

--- decoding/exports.ts
+++ decoding/exports.ts
@@ -1,11 +1,12 @@
+import { varintDecode } from "./varint.ts";
+import { decodeString } from "./util.ts";
+import { DecodingError, ValidationError } from "../error.ts";
 import type { Result } from "../types/common.ts";
 import type {
   DecodedExport,
   DecodedModule,
   ExternalKind,
 } from "../types/module/decoded.ts";
-import { varintDecode } from "./varint.ts";
-import { decodeString } from "./util.ts";
 
 const BYTE_TO_EXTERNAL_KIND: ExternalKind[] = [
   "func",
@@ -14,13 +15,23 @@ const BYTE_TO_EXTERNAL_KIND: ExternalKind[] = [
   "global",
 ];
 
+const EXTERNAL_KIND_VALIDATOR: Record<
+  ExternalKind,
+  (module: DecodedModule, e: DecodedExport) => boolean
+> = {
+  func: (module, exp) => exp.index < module.funcs.length,
+  table: (module, exp) => exp.index < module.tables.length,
+  memory: (module, _exp) => !!module.memory,
+  global: (module, exp) => exp.index < module.globals.length,
+};
+
 function decodeExport(bytes: Uint8Array): Result<DecodedExport> {
   const name = decodeString(bytes);
   const kind = BYTE_TO_EXTERNAL_KIND[bytes[name.bytesConsumed]];
 
   bytes = bytes.subarray(name.bytesConsumed + 1);
 
-  if (!kind) throw new Error("Unknwon External kind");
+  if (!kind) throw new DecodingError("Unknown External kind");
 
   const [index, consumed] = varintDecode(bytes);
 
@@ -40,15 +51,18 @@ export function decodeExportSection(module: DecodedModule, bytes: Uint8Array) {
   bytes = bytes.subarray(offset);
 
   for (let i = 0; i < count; i++) {
-    const result = decodeExport(bytes);
-    if (
-      result.value.kind === "memory" &&
-      (module.memory === null || result.value.index !== 0)
-    ) {
-      throw new Error("Cannot export undefined memory");
+    const { value, bytesConsumed } = decodeExport(bytes);
+
+    if (module.exports.some((e) => e.exportName === value.exportName)) {
+      throw new ValidationError("Duplicate export name");
     }
 
-    module.exports.push(result.value);
-    bytes = bytes.subarray(result.bytesConsumed);
+    const isValid = EXTERNAL_KIND_VALIDATOR[value.kind];
+    if (!isValid(module, value)) {
+      throw new ValidationError("Index out of bound");
+    }
+
+    module.exports.push(value);
+    bytes = bytes.subarray(bytesConsumed);
   }
 }
